43a4b2b4378db73ce86a7a8187ac5c1d43045b63
erikawinv <erika.sallberg@winventtogether.com>
2025-09-24 15:17:55 +0200

    feat: added methods for entering the lift

--- lift/src/lift/Monitor.java
+++ lift/src/lift/Monitor.java
@@ -1,5 +1,55 @@
 package lift;
 
-public class Monitor {
+import java.util.ArrayList;
 
+public class Monitor {
+    private static final int MAX_LOAD = 4;
+
+    private int entering = 0;
+    private int load = 0;
+
+    private ArrayList<Person> loadedPersons = new ArrayList<>();
+    private ArrayList<Person> waitingPersons = new ArrayList<>();
+
+    // check if lift is full
+    private boolean liftFull() {
+        return load >= MAX_LOAD;
+    }
+
+    // check if person can enter by checking if lift is full
+    private boolean personCanEnter(Person person) {
+        return !liftFull();
+    }
+
+    // adds person to the waiting list for entering the lift
+    public void addWaitingPerson(Person person) {
+        waitingPersons.add(person);
+    }
+
+    // adds person to the list with persons that is curently in the lift
+    private void enterLift(Person person) {
+        loadedPersons.add(person);
+
+    }
+
+    // if person can't enter -> wait, else let oerson enter lift
+    public void enterWhenAllowed(Person person) throws InterruptedException {
+        while (!personCanEnter(person)) {
+            wait();
+        }
+        enterLift(person);
+        entering++;
+    }
+
+    public void exitWhenAllowed(Person person) {
+    }
+
+    // delete person that just entered from list of persons who want's to enter
+    public void entered(Person person) {
+
+    }
+
+    public void exited(Person person) {
+
+    }
 }
--- lift/src/lift/Person.java
+++ lift/src/lift/Person.java
@@ -2,7 +2,6 @@ package lift;
 
 import java.util.Random;
 
-import javax.management.monitor.Monitor;
 //each Person is a Passenger that needs to have a Monitor too  
 
 public class Person extends Thread {
@@ -19,32 +18,44 @@ public class Person extends Thread {
     @Override
     public void run() { // will be called by start()
         // should be delayed "delay the animate passengers walk"
-        // should enter if allowed
-        // should exit if allowed
+        try {
+            delay(45);
+            begin();
+            // should enter if allowed
+            waitAndEnter();
+            // should exit if allowed
+            waitAndExit();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void delay(int maxDelaySeconds) {
+    private void delay(int maxDelaySeconds) throws InterruptedException {
         Random rand = new Random();
         int timeDelayed = rand.nextInt(maxDelaySeconds);
-
+        Thread.sleep(timeDelayed * 1000); // sleep takes milliseconds, thats why * 1000
     }
 
     private void begin() {
         passenger.begin();
-        // have to add wainting person
+        monitor.addWaitingPerson(this);
     }
 
-    private void waitAndEnter() {
+    private void waitAndEnter() throws InterruptedException {
         // passenger should enter when allowed - monitor should keep track of that
+        monitor.enterWhenAllowed(this);
+        // passenger enters lift
         passenger.enterLift();
         // monitor should register that passenger entered
+        monitor.entered(this);
     }
 
     private void waitAndExit() {
         // monitor should keep track of when passenger can exit
+        monitor.exitWhenAllowed(this);
         passenger.exitLift();
         // monitor should register when person, passenger, has left
-
+        monitor.exited(this);
     }
 
 }
